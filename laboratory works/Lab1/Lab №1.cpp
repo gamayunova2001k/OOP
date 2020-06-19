ef5773b30853fdf477d878bc64c10015ea484726
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2020-06-19 15:39:20 +0300

    Add files via upload

---
+++ laboratory works/Lab1/Lab №1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "MyClass.h"
+
+/*
+	Дано:
+Перечисление ключевых слов и сайтов,
+на которых встречается каждое из них
+
+	Получить:
+Набор сайтов, где встречается
+(не встречается) указанное подмножество
+ключевых слов
+*/
+
+using namespace System;
+using namespace System::Collections;
+
+int main(array<System::String^>^ args)
+{
+	MyClass ClassObj;
+	ClassObj.AddNameToList("www.people-rights.ru/Alliance");
+	ClassObj.AddNameToList("www.people-rights.ru/Union");
+	ClassObj.AddNameToList("www.people-rights.ru/Ruts");
+	ClassObj.AddNameToList("www.tea-house.ru/Dark");
+	ClassObj.AddNameToList("www.tobacco.ru/Dark");
+	ClassObj.AddNameToList("www.people-rights.ru/Dark");
+	ClassObj.AddNameToList("www.tobacco.ru/Side");
+	ClassObj.AddNameToList("www.people-rights.ru/Side");
+	ClassObj.AddNameToList("www.tobacco.ru/Musthave");
+	ClassObj.AddNameToList("www.tea-house.ru/Curtis");
+	ClassObj.AddNameToList("www.tea-house.ru/Greenfield");
+	ClassObj.AddNameToList("www.tea-house.ru/Tess");
+	ClassObj.SaveToSitesWithWords("Dark");
+	ClassObj.SaveToSitesWithoutWords("Dark");
+	return 0;
+}
---
+++ laboratory works/Lab1/MyClass.cpp
@@ -0,0 +1,83 @@
+#include "pch.h"
+#include "Windows.h"
+#include "MyClass.h"
+
+using namespace System;
+using namespace System::Collections;
+
+MyClass::MyClass(void) 
+{
+	TextFileName1 = gcnew String("FileIncluded.txt");
+	TextFileName2 = gcnew String("FileNotIncluded.txt");
+	aNames = gcnew ArrayList();
+}
+
+ArrayList^ MyClass::GetSitesWithWords(String^ Words) 
+{
+	ArrayList^ aNameList = gcnew ArrayList();
+	for (int i = 0; i < aNames->Count; i++) {
+		String^ e = (String^)aNames[i];
+		if (e->IndexOf(Words) > -1) {
+			aNameList->Add(e);
+		}
+	}
+	return(aNameList);
+}
+
+ArrayList^ MyClass::GetSitesWithoutWords(String^ Words) 
+{
+	ArrayList^ aNameList = gcnew ArrayList();
+	for (int i = 0; i < aNames->Count; i++) {
+		String^ e = (String^)aNames[i];
+		if (e->IndexOf(Words) == -1) {
+			aNameList->Add(e);
+		}
+	}
+	return(aNameList);
+}
+
+void MyClass::SetTextFileName(String^ NewName) 
+{
+	TextFileName1 = NewName;
+	TextFileName2 = NewName;
+}
+
+void MyClass::SaveToSitesWithWords(String^ Words) 
+{
+	ArrayList^ aNameList = gcnew ArrayList();
+	aNameList = GetSitesWithWords(Words);
+	System::Console::WriteLine("Sites that include this word: \n");
+	for (int i = 0; i < aNameList->Count; i++) {
+		System::Console::WriteLine((String^)aNameList[i]);
+	}
+	System::IO::StreamWriter^ sw = gcnew System::IO::StreamWriter(this->TextFileName1);
+	for (int i = 0; i < aNameList->Count; i++) {
+		sw->WriteLine((String^)aNameList[i]);
+	}
+	sw->Close();
+}
+
+void MyClass::SaveToSitesWithoutWords(String^ Words) 
+{
+	ArrayList^ aNameList = gcnew ArrayList();
+	aNameList = GetSitesWithoutWords(Words);
+	System::Console::WriteLine("\nSites that not include this word: \n");
+	for (int i = 0; i < aNameList->Count; i++) {
+		System::Console::WriteLine((String^)aNameList[i]);
+	}
+	System::IO::StreamWriter^ sw = gcnew System::IO::StreamWriter(this->TextFileName2);
+	for (int i = 0; i < aNameList->Count; i++) {
+		sw->WriteLine((String^)aNameList[i]);
+	}
+	sw->Close();
+}
+
+void MyClass::AddNameToList(String^ Name) 
+{
+	aNames->Add(Name);
+}
+
+void MyClass::DelNameFromList(String^ Name) 
+{
+	aNames->Remove(Name);
+}
